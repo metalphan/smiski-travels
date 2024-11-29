1cda2c2d6b3dc3ca5260b89e7ed6a4712f32ec27
Gabriel L <47095962+metalphan@users.noreply.github.com>
2024-11-29 14:39:18 -0800

    commiting crossing my fingers

--- smiski-travels/fetch_images.rb
+++ smiski-travels/fetch_images.rb
@@ -1,21 +1,46 @@
-# Created 2024-11-24 with lots of elp from Github and Gemini
+# Created 2024-11-24 with lots of help from Github and Gemini
 
 require 'net/http'
 require 'json'
+require 'fileutils'
+require 'open-uri'
 
-def fetch_images(url, output_file)
+def fetch_images(url, output_file, photos_dir)
   uri = URI(url)
   response = Net::HTTP.get_response(uri)
 
   if response.code == '200'
     image_data = JSON.parse(response.body)
 
-    # Open the output JSON file for writing
+    # Save the metadata to the output JSON file
     File.open(output_file, 'w') do |file|
       file.write(JSON.pretty_generate(image_data))
     end
+    puts "Image metadata has been successfully written to #{output_file}."
 
-    puts "Image data has been successfully written to #{output_file}."
+    # Ensure the photos directory exists
+    FileUtils.mkdir_p(photos_dir)
+
+    # Download each image to the photos directory
+    image_data.each do |image|
+      image_url = image["url"]
+      filename = image["filename"]
+      destination_path = File.join(photos_dir, filename)
+
+      puts "Downloading image: #{image_url} to #{destination_path}"
+
+      begin
+        # Use OpenURI to download the image and save it
+        URI.open(image_url) do |remote_file|
+          File.open(destination_path, 'wb') do |local_file|
+            local_file.write(remote_file.read)
+          end
+        end
+        puts "Image downloaded successfully: #{destination_path}"
+      rescue StandardError => e
+        puts "Error downloading image: #{e.message}"
+      end
+    end
   else
     puts "Error fetching images: #{response.code}"
     puts response.body
@@ -25,7 +50,10 @@ end
 # Replace with your Cloudflare Worker API endpoint
 url = 'https://smiski-photos-worker.metalphan.workers.dev/?album_id=ALbRIU_103UAj7do5yTn8_h49i8AlymjoWa6Oa9UQFMRYr9uM_aWe5r3QP1o4C3dLSAFdcVFshRz4wD3gvrQLJe6PxNPsWml3w'
 
-# Output file to save the JSON data
+# Output file for metadata
 output_file = 'import_data.json'
 
-fetch_images(url, output_file)
+# Photos directory relative to the script
+photos_dir = 'photos'
+
+fetch_images(url, output_file, photos_dir)
